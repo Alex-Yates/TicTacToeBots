14fccb3cabed27c80770e1d5cc2f4c1c7c7a4c88
James Hutchinson <j@meshut.ch>
2015-08-30 15:47:48 +0100

    Add breeding constructor to GeneBot

--- src/ttt/bots/GeneBot.java
+++ src/ttt/bots/GeneBot.java
@@ -6,12 +6,15 @@ import java.util.Random;
 
 public class GeneBot implements iPlayer {
 
+    private static final int NUM_RULES = 2000;
+    private static final float MUT_PROB = 0.005f;
+
     private List<Integer>  _rules;
 
     public GeneBot() {
-        _rules = new ArrayList<Integer>(2000);
+        _rules = new ArrayList<Integer>(NUM_RULES);
         Random rand = new Random();
-        for (int i = 0; i < 2000; i++) {
+        for (int i = 0; i < NUM_RULES; i++) {
             _rules.add(rand.nextInt());        
         }
     }
@@ -20,17 +23,61 @@ public class GeneBot implements iPlayer {
         _rules = rules;
     }
 
-    private boolean ruleMatch(int board, int rule){
+    public GeneBot(GeneBot mother, GeneBot father) {
+        Random rand = new Random();
+        _rules = new ArrayList<Integer>(NUM_RULES);
+
+        for (int i = 0; i < NUM_RULES; i++) {
+            if (rand.nextBoolean()) {
+                _rules.add(mother.getRule(i));
+            } else {
+                _rules.add(father.getRule(i));
+            }
+        }
+
+        for (int i = 0; i < NUM_RULES; i++) {
+            int mutation = 0;
+            for (int j = 0; j < 32; j++) {
+                if (rand.nextFloat() < MUT_PROB) {
+                    mutation = mutation + (1 << i);
+                }
+            }
+            _rules.set(i, _rules.get(i) ^ mutation);
+        }
+    }
+
+    private boolean ruleMatch(int board, int rule) {
         return (rule & board & 0xFFFFFFE0) == 0;
     } 
 
     public int takeTurn(int board) {
         for (Integer rule : _rules) {
-            if (ruleMatch(board, rule)){
+            if (ruleMatch(board, rule)) {
                 return rule & 0xF;
             }
         }
-        return 0;
+        return 15;
+    }
+
+    public int getRule(int i) {
+        return _rules.get(i);
+    }
+
+    public void printRules() {
+        for (Integer rule : _rules) {
+            System.out.println(Utils.toString(rule));
+        }
+    }
+
+    public static void main(String[] args) {
+        GeneBot parent1 = new GeneBot();
+        GeneBot parent2 = new GeneBot();
+        GeneBot baby = new GeneBot(parent1,parent2);
+        parent1.printRules();
+        System.out.println("");
+        parent2.printRules();
+        System.out.println("");
+        baby.printRules();
     }
 
 }

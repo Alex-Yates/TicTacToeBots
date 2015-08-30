a30e649a6f43368a933a23bb92dbbab02e5e3dce
Alex-Yates <ajy.mail@gmail.com>
2015-08-30 15:26:18 +0100

    Improvements to game class
    More efficient
    Persistent counter of total legal moves
    Game returns the winner
    Game is re-runnable and initialises each time

--- src/ttt/bots/Game.java
+++ src/ttt/bots/Game.java
@@ -5,6 +5,8 @@
  */
 package ttt.bots;
 
+import java.util.Random;
+
 
 /**
  *
@@ -12,10 +14,14 @@ package ttt.bots;
  */
 public class Game {
 
-    private iPlayer _crossPlayer = new GeneBot();
-    private iPlayer _noughtPlayer = new GeneBot();
-    private int _gameState = TTT.NEWBOARD;
-    private iPlayer _currentPlayer = _crossPlayer;
+    private iPlayer _crossPlayer;
+    private iPlayer _noughtPlayer;
+    private int _crossPlayerScore = 0;
+    private int _noughtPlayerScore = 0;
+    private int _totalLegalMoves;
+    private int _gameState;
+    private iPlayer _currentPlayer;// = _crossPlayer;
+    private Random _random = new Random();
 
     private void nextPlayer() {
         if (_currentPlayer == _crossPlayer) 
@@ -32,7 +38,7 @@ public class Game {
         // Checking the square is between 0 and 8 
         if (square > 8 || square < 0) 
         {
-            System.out.println("Square " + square + " does notr exist! Must be 0-8.");
+            System.out.println("Square " + square + " does not exist! Must be 0-8.");
             return false;
         }
 
@@ -47,27 +53,76 @@ public class Game {
         return true;
     }
 
-    private boolean gameEnd() {
-        for (int i = 0; i < 8; i++)
+
+    
+    private boolean crossWin()
         {
-            if ((TTT.getCrossVictory(i) & _gameState) == 0)
+            for (int i = 0; i < 8; i++)
             {
-                System.out.println("Crosses have won!");
-                return true;
+                if ((TTT.getNoughtVictory(i) & _gameState) == 0)
+                {
+                    System.out.println("Noughts have won!");
+                    return true;
+                }
             }
+            return false;
         }
-        for (int i = 0; i < 8; i++)
+    
+    private boolean noughtWin()
         {
-            if ((TTT.getNoughtVictory(i) & _gameState) == 0)
+            for (int i = 0; i < 8; i++)
             {
-                System.out.println("Noughts have won!");
-                return true;
+                if ((TTT.getNoughtVictory(i) & _gameState) == 0)
+                {
+                    System.out.println("Noughts have won!");
+                    return true;
+                }
             }
+            return false;
         }
-        return false;
-    }
+    
+//    private boolean gameEnd() {
+//        for (int i = 0; i < 8; i++)
+//        {
+//            if ((TTT.getCrossVictory(i) & _gameState) == 0)
+//            {
+//                System.out.println("Crosses have won!");
+//                return true;
+//            }
+//        }
+//        for (int i = 0; i < 8; i++)
+//        {
+//            if ((TTT.getNoughtVictory(i) & _gameState) == 0)
+//            {
+//                System.out.println("Noughts have won!");
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
     private void playMove(iPlayer bot) 
+    {
+        int square = _currentPlayer.takeTurn(_gameState);
+        System.out.println(" I am chosing square " + square);
+        if (isValidMove(square)) 
+        {
+            _gameState &= ~TTT.getMask(square);
+            
+            if (bot == _crossPlayer) 
+            {
+                _gameState = (_gameState + TTT.getCross(square));
+                _crossPlayerScore++;
+            }
+            else
+            {
+                _gameState = (_gameState + TTT.getNought(square));
+                _noughtPlayerScore++;
+            }     
+        }
+    }
+    
+    private void playLastMove(iPlayer bot) 
     {
         int square = _currentPlayer.takeTurn(_gameState);
         System.out.println(" I am chosing square " + square);
@@ -86,29 +141,105 @@ public class Game {
         }
     }
 
-    private void playGame() 
-    {
-        for (int i = 0; i < 9; i++) 
-        {
-            playMove(_currentPlayer);
-            
-            Utils.printBoard(_gameState);
-            System.out.println(Utils.toString(_gameState));
-            
-            if (gameEnd()) 
-            {
-                break;
-            }
-            nextPlayer();
-        }
 
-    }
+    private iPlayer playGame(iPlayer crossPlayer, iPlayer noughtPlayer) 
+    {
+        _gameState = TTT.NEWBOARD;
+        _crossPlayer = crossPlayer;
+        _noughtPlayer = noughtPlayer;
+        _currentPlayer = crossPlayer;
+        _totalLegalMoves = _totalLegalMoves + _crossPlayerScore + _noughtPlayerScore;
+        _crossPlayerScore = 0;
+        _noughtPlayerScore = 0;
+        
+        // first turn
+        playMove(crossPlayer);
+        Utils.printBoard(_gameState);
+        System.out.println(Utils.toString(_gameState));
+        
+        // second turn
+        playMove(noughtPlayer);
+        Utils.printBoard(_gameState);
+        System.out.println(Utils.toString(_gameState));        
+        
+        // third turn
+        playMove(crossPlayer);
+        Utils.printBoard(_gameState);
+        System.out.println(Utils.toString(_gameState));
+        
+        // fourth turn
+        playMove(noughtPlayer);
+        Utils.printBoard(_gameState);
+        System.out.println(Utils.toString(_gameState));        
+        
+        // fifth turn
+        playMove(crossPlayer);
+        Utils.printBoard(_gameState);
+        System.out.println(Utils.toString(_gameState));
+        if (crossWin())
+        {
+            return crossPlayer;
+        }
+        
+        // sixth turn
+        playMove(noughtPlayer);
+        Utils.printBoard(_gameState);
+        System.out.println(Utils.toString(_gameState));        
+        if (noughtWin())
+        {
+            return noughtPlayer;
+        }
+        
+        // seventh turn
+        playMove(crossPlayer);
+        Utils.printBoard(_gameState);
+        System.out.println(Utils.toString(_gameState));
+        if (crossWin())
+        {
+            return crossPlayer;
+        }
+        
+        // eigth turn
+        playMove(noughtPlayer);
+        Utils.printBoard(_gameState);
+        System.out.println(Utils.toString(_gameState));        
+        if (noughtWin())
+        {
+            return noughtPlayer;
+        }
+        
+        // ninth turn
+        playLastMove(crossPlayer);
+        Utils.printBoard(_gameState);
+        System.out.println(Utils.toString(_gameState));
+        if (crossWin())
+        {
+            return crossPlayer;
+        }
+        
+        // if no winner return player with most legal moves
+        if (_crossPlayerScore > _noughtPlayerScore)
+        {
+            return crossPlayer;
+        }
+        
+        if (_crossPlayerScore < _noughtPlayerScore)
+        {
+            return noughtPlayer;
+        }
+        
+        // if no winner return random player
+        return _random.nextBoolean() ? crossPlayer : noughtPlayer;
+    }    
     
     public static void main(String[] args)
     {
         Game g = new Game();
   
-        g.playGame();
+        iPlayer crossPlayer = new GeneBot();
+        iPlayer noughtPlayer = new GeneBot();
+        
+        iPlayer winner = g.playGame(crossPlayer, noughtPlayer);
     }
 
 }

0070545334869a374056014dde15df1dfc92abb9
Alex-Yates <ajy.mail@gmail.com>
2015-08-31 17:48:56 +0100

    Attempting to log all data to .csv not working yet
    - Only logging forst line, some overwriting is happening
    - Perhaps its better to use a proper logging library?

--- src/ttt/bots/Generation.java
+++ src/ttt/bots/Generation.java
@@ -5,7 +5,12 @@
  */
 package ttt.bots;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
@@ -18,7 +23,6 @@ public class Generation {
     // member variuables to hold all our bots
     private List<GeneBot> _contestants = new ArrayList<>();
     private List<GeneBot> _winners = new ArrayList<>();
-
     
     // constructors
     public Generation()
@@ -42,8 +46,15 @@ public class Generation {
         _contestants.addAll(bots);
     }
     
-    private List<GeneBot> runGeneration()
+    private List<GeneBot> runTournament(String logFilePath)
     {
+        // ensuring there are an even number of contestants
+        if ((_contestants.size() % 2) == 1)
+        {
+            _contestants.remove(_contestants.size() - 1);
+        }
+        
+        // killing the weak
         Game game = new Game();
         for (int i = 0; i < _contestants.size(); i+=2)
         {
@@ -53,42 +64,146 @@ public class Generation {
             iPlayer winner = game.playGame(crossPlayer, noughtPlayer);
             _winners.add((GeneBot)winner);
         }
-        System.out.println("Total legal moves: " + game.getTotalLegalMoves());
-        System.out.println("Total failed moves: " + game.getTotalFailedMoves());
-        System.out.println("Total Cross wins: " + game.getTotalCrossWins());
-        System.out.println("Total Nought wins: " + game.getTotalNoughtWins());
-        System.out.println("Total number of winners: " + _winners.size());
-        System.out.println("Total number of contestants: " + _contestants.size());
-        System.out.println("*************");
+        
+        // logging stats
+        saveStats(game.getTotalLegalMoves(), game.getTotalFailedMoves(),
+                  game.getTotalCrossWins(), game.getTotalNoughtWins(),
+                  _winners.size(), _contestants.size(), logFilePath);
+       
         return _winners;
     }
     
-    public void regenerate()
+    private void runMultiRoundTournament(int rounds, String logFilePath)
+    {
+        // creating a .csv file to log results to
+        try
+	{
+	    FileWriter writer = new FileWriter(logFilePath);
+            
+            writer.append("Total legal moves");
+	    writer.append(',');
+	    writer.append("Total failed moves");
+            writer.append(',');
+	    writer.append("Legal move rate");	    
+            writer.append(',');
+	    writer.append("Total Cross wins");
+            writer.append(',');
+	    writer.append("Total Nought wins");
+            writer.append(',');
+	    writer.append("Total number of winners");
+            writer.append(',');
+	    writer.append("Total number of contestants");
+            writer.append('\n');
+            
+            writer.flush();
+	    writer.close();
+        }    
+	catch(IOException e)
+	{
+	     e.printStackTrace();
+	}
+        
+        
+        // run multiple generations, killing the weakest
+        // 50% each time
+        for (int r = 0; r < rounds; r++)
+        {
+            System.out.println("Round: " + r);
+            _winners.clear();
+            runTournament(logFilePath);
+            _contestants.clear();
+            _contestants.addAll(_winners);
+ 
+        }
+        
+        // spawn population back to original size
+        Regenerate(rounds);      
+    }
+    
+    public void Regenerate(int spawnRate)
     {
         List<GeneBot> children = new ArrayList<>();
         
-            for (int i = 0; i < _winners.size(); i+=2)
-            {
-                children.add(new GeneBot(_winners.get(i), _winners.get(i+1)));
-                children.add(new GeneBot(_winners.get(i), _winners.get(i+1)));
-             }
-            _contestants.clear();
-            _contestants.addAll(_winners);
-            _contestants.addAll(children);
-            _winners.clear();    
+        // ensuring there are an even number of winners
+        if ((_winners.size() % 2) == 1)
+        {
+            _winners.remove(_winners.size() - 1);
+        } 
+        
+        // repopulating contestants back to original size
+        // note, it is possible the size will reduce slightly
+        // due to odd numbers
+        for (int numKids = 0; numKids < Math.pow(2, spawnRate); numKids++)
+        {
+            for (int i = 0; i < (_winners.size()); i+=2)
+            {   
+                children.add(new GeneBot(_winners.get(i), _winners.get(i+1)));                
+                children.add(new GeneBot(_winners.get(i), _winners.get(i+1))); 
+            }
+        }   
+
+        _contestants.clear();
+        _contestants.addAll(_winners);
+        _contestants.addAll(children);
+        _winners.clear();    
+    }
+    
+    private void saveStats(int legalMoves, int failedMoves, 
+                           int totalCrossWins, int totalNoughtWins,
+                           int totalWinners, int totalContestants,
+                           String logFilePath)
+    {      
+        System.out.println("Total legal moves: " + legalMoves);
+        System.out.println("Total failed moves: " + failedMoves);
+        double hitRate = ((double)legalMoves / ((double)failedMoves + (double)legalMoves));
+        System.out.println("Legal move rate: " + hitRate);
+        System.out.println("Total Cross wins: " + totalCrossWins);
+        System.out.println("Total Nought wins: " + totalNoughtWins);
+        System.out.println("Total number of winners: " + totalWinners);
+        System.out.println("Total number of contestants: " + totalContestants);
+        System.out.println("*************");
+        
+               // creating a .csv file to log results to
+        try
+	{
+	    FileWriter writer = new FileWriter(logFilePath);
+            
+            writer.append('\n');
+            writer.append(Integer.toString(legalMoves));
+	    writer.append(',');
+	    writer.append(Integer.toString(failedMoves));
+            writer.append(',');
+	    writer.append(Double.toString(hitRate));	    
+            writer.append(',');
+	    writer.append(Integer.toString(totalCrossWins));
+            writer.append(',');
+	    writer.append(Integer.toString(totalNoughtWins));
+            writer.append(',');
+	    writer.append(Integer.toString(totalWinners));
+            writer.append(',');
+	    writer.append(Integer.toString(totalContestants));
+
+            
+            writer.flush();
+	    writer.close();
+        }    
+	catch(IOException e)
+	{
+	     e.printStackTrace();
+	}
     }
     
     public static void main(String[] args)
     {
-        Generation g = new Generation(500, 0.005f);
-  
-        g.runGeneration();
+        Generation g = new Generation(500, 0.05f);
+        String logFile = new SimpleDateFormat("'C:\\Users\\Alex\\Documents\\TicTacToeLogs'yyyyMMddhhmmss'.csv'").format(new Date());
+ 
         for (int i = 0; i < 5000; i++)
         {
-            System.out.println("Generation number: " + i);
-            g.regenerate();
-            g.runGeneration();
+        System.out.println("Tournament number: " + i);
+        g.runMultiRoundTournament(5, logFile);
         }
+
     }
     
 }
